11de74d67d934e383bc3018824c2bc4ebb4557fe
merovic <amirahmedmorsy@yahoo.com>
2019-10-26 13:39:57 +0200

    Initial Commit

---
+++ fastini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fastini
+//
+//  Created by Apple on 10/26/19.
+//  Copyright © 2019 amirahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
